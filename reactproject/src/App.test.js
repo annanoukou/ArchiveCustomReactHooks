6c97b3bf1186d25db5ed30024285a804866f8f02
annanoukou <anna.noukou@gmail.com>
2022-10-23 14:54:00 +0300

    test

--- reactproject/src/App.test.js
+++ reactproject/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn ojo', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/Keep Numbers/i);
     expect(linkElement).toBeInTheDocument();
 })
